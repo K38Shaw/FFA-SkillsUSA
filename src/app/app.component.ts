e7c2f9bdf4244e44dd6f50c9a992e7f44dfa9266
Kenli Shaw <kenli.shaw@nwktc.edu>
2023-02-13 14:20:21 -0700

    built initial pieces for each page. Event Schedule CSS done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,9 @@ export class AppComponent {
   
 
 
-  
+  alertFail(){
+    alert("Attempt Unsuccessful")
+  }
   alertButton(){
     alert("I have been pressed.")}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import {MatTableModule} from '@angular/material/table';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
+
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StudentDataComponent } from './student-data/student-data.component';
 import { SchoolDataComponent } from './school-data/school-data.component';
 import { ReadDataComponent } from './read-data/read-data.component';
+import { EventScheduleComponent } from './event-schedule/event-schedule.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { ReadDataComponent } from './read-data/read-data.component';
     NotesComponent,
     StudentDataComponent,
     SchoolDataComponent,
-    ReadDataComponent
+    ReadDataComponent,
+    EventScheduleComponent
   ],
   imports: [
     BrowserModule,
